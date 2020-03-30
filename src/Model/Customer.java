231dce8d7d66fd20516932a65a1cc32278b37cdf
Hadra94 <60923988+Hadra94@users.noreply.github.com>
2020-03-30 13:10:47 +0200

    mo

--- src/Model/Customer.java
+++ src/Model/Customer.java
@@ -2,5 +2,76 @@ package Model;
 
 public class Customer {
 
-    public static int id = 0;
+    private String Name;
+    private int phoneNo;
+    private String Mail;
+    private int customerNo;
+    private int prevOrder;
+
+    //Getter and Setter
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(int phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getMail() {
+        return Mail;
+    }
+
+    public void setMail(String mail) {
+        Mail = mail;
+    }
+
+    public int getCustomerNo() {
+        return customerNo;
+    }
+
+    public void setCustomerNo(int customerNo) {
+        this.customerNo = customerNo;
+    }
+
+    public int getPrevOrder() {
+        return prevOrder;
+    }
+
+    public void setPrevOrder(int prevOrder) {
+        this.prevOrder = prevOrder;
+    }
+
+    //Construktor
+
+    public Customer(String name, int phoneNo, String mail, int customerNo, int prevOrder) {
+        Name = name;
+        this.phoneNo = phoneNo;
+        Mail = mail;
+        this.customerNo = customerNo;
+        this.prevOrder = prevOrder;
+    }
+
+    //toString
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "Name='" + Name + '\'' +
+                ", phoneNo=" + phoneNo +
+                ", Mail='" + Mail + '\'' +
+                ", customerNo=" + customerNo +
+                ", prevOrder=" + prevOrder +
+                '}';
+    }
 }
+
+
